2a97fc0b1b55e9c4db542557713890bf5b3744d7
Mihir <mihirniyogi942@gmail.com>
2023-05-07 16:48:25 +0800

    code cleanup

--- main.py
+++ main.py
@@ -207,9 +207,6 @@ def ACTIVE_MODE(colour,difficulty):
                 else:        
                     SCREEN.blit(image,image.get_rect(center=coord))
 
-    def isValidMove(move):
-        return sf.is_move_correct(move) 
-
     # if user's colour is black, computer makes the first move 
     if colour == 'BLACK':
         computer_move = sf.get_best_move_time(200)
@@ -242,73 +239,80 @@ def ACTIVE_MODE(colour,difficulty):
         # check for moves
         for sq_sprite in square_group:
             
+            # check if the square is clicked on AND has a piece attached to it
             clicked = sq_sprite.check_clicked(event_list)
             
             if sq_sprite.piece and clicked:
-                
+
+                # set the "move" variables    
                 move_mode = "DOWN"
                 move_piece = clicked[0]
                 move_piece_image = clicked[1]
                 move_square_source = clicked[2]
                 move_square_sprite_source = sq_sprite
 
+            # which square the mouse has been unclicked on? (to make the move)
             unclicked = sq_sprite.check_unclicked(event_list)
             
-            if unclicked:
+            if unclicked and move_mode == "DOWN":
+                    
+                move_mode = "UP"
+                move_square_dest = unclicked[2]
+                move_square_sprite_dest = sq_sprite
+                move = move_square_source+move_square_dest
                 
-                if move_mode == "DOWN":
-                    
-                    move_mode = "UP"
-                    move_square_dest = unclicked[2]
-                    move_square_sprite_dest = sq_sprite
-                    
-                    move = move_square_source+move_square_dest
-
-                    if isValidMove(move):
-                        print("h")
-                        # play move
-                        true_board_dict[move_square_source] = ""
-                        true_board_dict[move_square_dest] = move_piece
-
-                        displayPiecesBasedOnTrueBoard()
-                        pygame.display.update()
-
-                        move_square_sprite_source.piece = ""
-                        move_square_sprite_source.piece_image = None
-
-                        move_square_sprite_dest.piece = move_piece
-                        move_square_sprite_dest.piece_image = move_piece_image
+                # if move is valid
+                if sf.is_move_correct(move):
                         
-                        sf.make_moves_from_current_position([move])
+                    # play move
+
+                    # set true board dictionary to new values. source to empty. dest to the piece moved
+                    true_board_dict[move_square_source] = ""
+                    true_board_dict[move_square_dest] = move_piece
+
+                    # update the display with the true board ** NOTE the dual call of pygame.display.update()
+                    displayPiecesBasedOnTrueBoard()
+                    pygame.display.update()
+
+                    # resetting the "move" variables
+                    move_square_sprite_source.piece = ""
+                    move_square_sprite_source.piece_image = None
+
+                    move_square_sprite_dest.piece = move_piece
+                    move_square_sprite_dest.piece_image = move_piece_image
                         
-                        print("Move:",move)
+                    sf.make_moves_from_current_position([move])
+                        
+                    print("Move:",move)
+                    print(sf.get_board_visual())
+                   
+                        
+                    # if, after the player moved, it's checkmate or stalemate, end the game, go to MENU
+                    eval = sf.get_evaluation()
+                    eval_type = eval["type"]
+                    if (eval_type == "mate") or len(sf.get_top_moves()) == 0:
+                        print("Checkmate/Stalemate")
+                        MENU_MODE()
+                    
+                    # if all is good (no mates), play computer move
+                    else:
+
+                        # play computer move
+                        computer_move = sf.get_best_move_time(200)
+                        sf.make_moves_from_current_position([computer_move])
+                        source = computer_move[0:2]
+                        dest = computer_move[2:4]
+                        piece_to_move = true_board_dict[source]
+                        true_board_dict[source] = ""
+                        true_board_dict[dest] = piece_to_move
                         print(sf.get_board_visual())
 
-                        
-                        eval = sf.get_evaluation()
-                        eval_type = eval["type"]
-                        
-                        if (eval_type == "mate") or len(sf.get_top_moves()) == 0:
-                            print("Checkmate/Stalemate")
-                            MENU_MODE()
-                        else:
-                            # play computer move
-
-                            computer_move = sf.get_best_move_time(200)
-                            sf.make_moves_from_current_position([computer_move])
-                            source = computer_move[0:2]
-                            dest = computer_move[2:4]
-                            piece_to_move = true_board_dict[source]
-                            true_board_dict[source] = ""
-                            true_board_dict[dest] = piece_to_move
-                            print(sf.get_board_visual())
-
-                    move_piece = ""
-                    move_piece_image = None
-                    move_square_source = ""
-                    move_square_dest = ""
-                    move_square_sprite_source = None
-                    move_square_sprite_dest = None
+                move_piece = ""
+                move_piece_image = None
+                move_square_source = ""
+                move_square_dest = ""
+                move_square_sprite_source = None
+                move_square_sprite_dest = None
 
         # based on true board dictionary, set the images of all pieces
         displayPiecesBasedOnTrueBoard()
